da641b93d51de5e4a6bc1a6c389dda84623ceef2
Kesterholtem21 <kesterholtem21@gcc.edu>
2024-02-19 14:29:45 -0500

    Main changes and gitignore

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Austin is having a hard time");
         System.out.println("He fixed it, YAY!");
         System.out.println("here is another change yay");
+        System.out.println("TO THE MASTER BRANCH");
     }
 
 
